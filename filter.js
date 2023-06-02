ac3325749cb3002cdbe9a81cf31b340a710c07f2
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-06-02 23:52:51 +0530

    feat: sentence-wise render blocking
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- filter.js
+++ filter.js
@@ -1,7 +1,8 @@
 // Prompt the user for a link
 const targetUrl = prompt("Enter a URL:"); // "https://en.wikipedia.org/wiki/Basketball" // 
+
 // Prompt the user for the blacklist word
-const blacklistWord = prompt("Enter the word to blacklist:"); // "basketball" //
+const blacklistWord = prompt("Enter the word to blacklist:"); // "basketball" // 
 
 // Fetch the URL and parse the response as text
 fetch(targetUrl)
@@ -16,17 +17,24 @@ fetch(targetUrl)
 
     // Loop through each paragraph and process/render accordingly
     paragraphs.forEach(paragraph => {
-      // Check if the paragraph contains the word "basketball"
-      if (!paragraph.textContent.toLowerCase().includes(blacklistWord.toLowerCase())) {
-        // Create a new paragraph element
-        const newParagraph = document.createElement("p");
-        newParagraph.textContent = paragraph.textContent;
+      // Create a new paragraph element
+      const newParagraph = document.createElement("p");
+      
+      // Split the paragraph into sentences
+      const sentences = paragraph.textContent.split('. ');
 
-        // Append the new paragraph to the body of the document
-        document.body.appendChild(newParagraph);
-      }
+      // Loop through each sentence and check if it contains the blacklist word
+      sentences.forEach(sentence => {
+        if (!sentence.toLowerCase().includes(blacklistWord.toLowerCase())) {
+          // Append the sentence to the new paragraph
+          newParagraph.textContent += sentence + '. ';
+        }
+      });
+
+      // Append the new paragraph to the body of the document
+      document.body.appendChild(newParagraph);
     });
   })
   .catch(error => {
-    console.error("Error fetching the Wikipedia article:", error);
+    console.error("Error fetching the URL:", error);
   });
