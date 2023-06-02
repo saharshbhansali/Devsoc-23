5160eebd590b3ebf389b810312299367f3ee64fd
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-06-02 23:45:12 +0530

    feat: script offload successful
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- filter.js
+++ filter.js
@@ -1,9 +1,9 @@
 // Prompt the user for a link
-const targetUrl = prompt("Enter a URL:");
+const targetUrl = prompt("Enter a URL:"); // "https://en.wikipedia.org/wiki/Basketball" // 
 // Prompt the user for the blacklist word
-const blacklistWord = prompt("Enter the word to blacklist:");
+const blacklistWord = prompt("Enter the word to blacklist:"); // "basketball" //
 
-// Fetch the target URL
+// Fetch the URL and parse the response as text
 fetch(targetUrl)
   .then(response => response.text())
   .then(data => {
@@ -11,38 +11,22 @@ fetch(targetUrl)
     const tempDiv = document.createElement("div");
     tempDiv.innerHTML = data;
 
-    // Get all the paragraphs from the target URL
+    // Get all the paragraphs from the article
     const paragraphs = tempDiv.querySelectorAll("p");
 
     // Loop through each paragraph and process/render accordingly
     paragraphs.forEach(paragraph => {
-      // Check if the paragraph contains the blacklist word
-      if (paragraph.textContent.toLowerCase().includes(blacklistWord.toLowerCase())) {
-        // Create a new div element to wrap the paragraph
-        const wrapperDiv = document.createElement("div");
+      // Check if the paragraph contains the word "basketball"
+      if (!paragraph.textContent.toLowerCase().includes(blacklistWord.toLowerCase())) {
+        // Create a new paragraph element
+        const newParagraph = document.createElement("p");
+        newParagraph.textContent = paragraph.textContent;
 
-        // Create a new span element to replace the paragraph
-        const hiddenContent = document.createElement("span");
-        hiddenContent.style.display = "none";
-        hiddenContent.innerHTML = paragraph.innerHTML;
-
-        // Create a button to reveal the hidden content
-        const revealButton = document.createElement("button");
-        revealButton.textContent = "Reveal";
-        revealButton.addEventListener("click", () => {
-          hiddenContent.style.display = "block";
-          revealButton.style.display = "none";
-        });
-
-        // Append the hidden content and reveal button to the wrapper div
-        wrapperDiv.appendChild(hiddenContent);
-        wrapperDiv.appendChild(revealButton);
-
-        // Replace the paragraph with the wrapper div
-        paragraph.parentNode.replaceChild(wrapperDiv, paragraph);
+        // Append the new paragraph to the body of the document
+        document.body.appendChild(newParagraph);
       }
     });
   })
   .catch(error => {
-    console.error("Error fetching the URL:", error);
+    console.error("Error fetching the Wikipedia article:", error);
   });
